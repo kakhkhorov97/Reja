0e9e4db7d5dec7030de88bb6c25bd4df743619c7
kakhkhorov97 <Kakhkhorov97@gmail.com>
2026-04-19 23:41:20 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log("Web server is running. . .");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish code
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded ({extended: true}));
+
+// 2 Session code
+
+// 3 Views code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+
+app.get("/hello", function (req, res) {
+    res.end("Hello World!");
+});
+
+app.get("/gift", function (req, res) {
+    res.end("This is a gift for you!");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is runnig successfully on PORT ${PORT}`);
+});
