6a279f597627fed116535c0640593575d7eb7605
kakhkhorov97 <Kakhkhorov97@gmail.com>
2026-04-28 22:15:44 +0900

    feat:MongoDB ulanish

---
+++ app.js
@@ -0,0 +1,84 @@
+// console.log("Web server is running. . .");
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+
+// // 1 Kirish code
+
+// app.use(express.static("public"));
+// app.use(express.json());
+// app.use(express.urlencoded ({extended: true}));
+
+// // 2 Session code
+
+// // 3 Views code
+
+// app.set("views", "views");
+// app.set("view engine", "ejs");
+
+// // 4 Routing code
+
+// app.get("/hello", function (req, res) {
+//     res.end("Hello World!");
+// });
+
+// app.get("/gift", function (req, res) {
+//     res.end("This is a gift for you!");
+// });
+
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function() {
+//     console.log(`The server is runnig successfully on PORT ${PORT}`);
+// });
+
+
+console.log("Web server is running. . .");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+// let user;
+// fs.readFile("database/user.json", "utf-8", (err, data) => {
+//     if (err) {
+//         console.log("Error:", err);
+//     }
+//     else {
+//         user = JSON.parse(data) 
+//     }
+// })
+
+//MongoDB chaqirish 
+const db = require("./server").db();
+
+// 1 Kirish code
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded ({extended: true}));
+
+// 2 Session code
+
+// 3 Views code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+});
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,86 +1,24 @@
-// console.log("Web server is running. . .");
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-
-// // 1 Kirish code
-
-// app.use(express.static("public"));
-// app.use(express.json());
-// app.use(express.urlencoded ({extended: true}));
-
-// // 2 Session code
-
-// // 3 Views code
-
-// app.set("views", "views");
-// app.set("view engine", "ejs");
-
-// // 4 Routing code
-
-// app.get("/hello", function (req, res) {
-//     res.end("Hello World!");
-// });
-
-// app.get("/gift", function (req, res) {
-//     res.end("This is a gift for you!");
-// });
-
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function() {
-//     console.log(`The server is runnig successfully on PORT ${PORT}`);
-// });
-
-
-console.log("Web server is running. . .");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if (err) {
-        console.log("Error:", err);
-    }
-    else {
-        user = JSON.parse(data) 
-    }
-})
+const mongodb = require("mongodb");
 
-// 1 Kirish code
+let db;
+const connectionString = "mongodb+srv://mitpractice1:mitpacticedatabase@cluster0.fruak8h.mongodb.net/Reja?appName=Cluster0"
+mongodb.connect(connectionString, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }, (err, client) => {
+        if (err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            module.exports = client;
+            const app = require ("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function() {
+             console.log(`The server is running successfully on PORT ${PORT}, http://localhost:${PORT}`);
+         });
+        }
+     }); 
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded ({extended: true}));
-
-// 2 Session code
-
-// 3 Views code
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-});
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is runnig successfully on PORT ${PORT}, http://localhost:${PORT}`);
-});
